68f82f111677d23eb7e9d44a23e345eac8e3d74e
leninner <mazabandalenin180@gmail.com>
2022-03-31 15:05:50 -0500

    Update Skeleton

--- src/components/SkeletonLoader/index.tsx
+++ src/components/SkeletonLoader/index.tsx
@@ -9,9 +9,31 @@ export const SkeletonLoader = () => {
     <>
       {skeletonItems.map((value: any) => (
         <JobInitialDataContainer key={value}>
-          <p>
-            <Skeleton />
-          </p>
+          <section className='JobMainInfo'>
+            <div className='BoxImage'>
+              <Skeleton width={90} height={90} />
+            </div>
+
+            <div className='JobData'>
+              <span>
+                <Skeleton width={150} height={17} />
+              </span>
+              <h3>
+                <Skeleton width={250} />
+              </h3>
+
+              <Skeleton width={66} height={22} />
+            </div>
+          </section>
+
+          <section className='DateInfo'>
+            <div>
+              <Skeleton width={100} height={17} />
+            </div>
+            <div>
+              <Skeleton width={100} height={17} />
+            </div>
+          </section>
         </JobInitialDataContainer>
       ))}
     </>
