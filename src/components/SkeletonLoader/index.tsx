4051bdd9aa9cc7eef64dd17760697c915074e785
leninner <mazabandalenin180@gmail.com>
2022-03-31 15:16:40 -0500

    Update Skeleton

--- src/components/SkeletonLoader/index.tsx
+++ src/components/SkeletonLoader/index.tsx
@@ -1,4 +1,4 @@
-import Skeleton from 'react-loading-skeleton';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { JobInitialDataContainer } from '../JobsList/styles';
 
@@ -9,31 +9,33 @@ export const SkeletonLoader = () => {
     <>
       {skeletonItems.map((value: any) => (
         <JobInitialDataContainer key={value}>
-          <section className='JobMainInfo'>
-            <div className='BoxImage'>
-              <Skeleton width={90} height={90} />
-            </div>
+          <SkeletonTheme baseColor='#ced4da' highlightColor='#dee2e6'>
+            <section className='JobMainInfo'>
+              <div className='BoxImage'>
+                <Skeleton width={90} height={90} />
+              </div>
 
-            <div className='JobData'>
-              <span>
-                <Skeleton width={150} height={17} />
-              </span>
-              <h3>
-                <Skeleton width={250} />
-              </h3>
+              <div className='JobData'>
+                <span>
+                  <Skeleton width={150} height={17} />
+                </span>
+                <h3>
+                  <Skeleton width={250} />
+                </h3>
 
-              <Skeleton width={66} height={22} />
-            </div>
-          </section>
+                <Skeleton width={66} height={22} />
+              </div>
+            </section>
 
-          <section className='DateInfo'>
-            <div>
-              <Skeleton width={100} height={17} />
-            </div>
-            <div>
-              <Skeleton width={100} height={17} />
-            </div>
-          </section>
+            <section className='DateInfo'>
+              <div>
+                <Skeleton width={100} height={17} />
+              </div>
+              <div>
+                <Skeleton width={100} height={17} />
+              </div>
+            </section>
+          </SkeletonTheme>
         </JobInitialDataContainer>
       ))}
     </>
