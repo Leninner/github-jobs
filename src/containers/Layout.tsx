90afd05d99944cd14b6111a5d12931119d586649
leninner <mazabandalenin180@gmail.com>
2022-04-04 17:47:32 -0500

    Added Framer Motion

--- src/containers/Layout.tsx
+++ src/containers/Layout.tsx
@@ -1,9 +1,14 @@
 import { MainTitle } from './styles';
+import { motion } from 'framer-motion';
 
 export const Layout = ({ children }: any) => {
   return (
     <div>
-      <MainTitle>
+      <MainTitle
+        as={motion.h1}
+        initial={{ y: '-100px' }}
+        animate={{ y: 0 }}
+        transition={{ duration: 0.5, ease: 'easeOut' }}>
         Github <span>Jobs</span>
       </MainTitle>
       {children}
